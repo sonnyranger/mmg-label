a82a6f7f40b075de45a7fa2831bc420023681b17
Dmitry <sevenkillshot@gmail.com>
2024-09-25 15:51:39 +0300

    9

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -45,7 +45,15 @@ const signOutMethod = async (): Promise<void> => {
 <template>
   <app-menubar :model="items" class="menu">
     <template #start>
-      <img src="/public/mmg.jpg" alt="logo" width="40" height="40" class="border-round-sm" to="/" />
+      <img
+        src="/public/mmg.jpg"
+        alt="logo"
+        width="40"
+        height="40"
+        class="border-round-sm"
+        cursor="pointer"
+        to="/"
+      />
     </template>
     <template #item="{ item, props }">
       <template v-if="item.show">
