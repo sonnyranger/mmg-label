bf446bc6192ccb8dc9dec0c65e20b27197057f15
Dmitry <sevenkillshot@gmail.com>
2024-09-25 15:33:27 +0300

    1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -44,6 +44,19 @@ const signOutMethod = async (): Promise<void> => {
 
 <template>
   <app-menubar :model="items" class="menu">
+    <template #start>
+      <svg
+        width="35"
+        height="40"
+        viewBox="0 0 35 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-8"
+      >
+        <path d="..." fill="var(--p-primary-color)" />
+        <path d="..." fill="var(--p-text-color)" />
+      </svg>
+    </template>
     <template #item="{ item, props }">
       <template v-if="item.show">
         <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
